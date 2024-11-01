289aa8e9a2363d04eb545908013e0a622a216d9e
George <georgehenrydeguzman@gmail.com>
2024-11-01 14:18:07 +0800

    Added Sprites

--- Scripts/options_pause_menu.gd
+++ Scripts/options_pause_menu.gd
@@ -1,14 +1,12 @@
 extends Control
 
 
-# Create volume sliders in the future :)
-func _on_sound_volume_pressed() -> void:
+func _on_sound_volume_2_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_music_volume_pressed() -> void:
+func _on_music_volume_2_pressed() -> void:
 	pass # Replace with function body.
 
-
-func _on_back_pressed() -> void:
+func _on_back_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
