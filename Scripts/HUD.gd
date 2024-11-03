bc531830bc17bcd85dd49c785387b0f405a97f9d
mintyZer0 <tantan.malonzo@gmail.com>
2024-11-03 15:48:10 +0800

    Added the doors and keys to the levels Transition between levels were added and the victory screen was also connected to the end of the level A lot of bugs on the UI, like the victory screen, quitting then going back to the main menu and some other stuff You'll find that you won't be able to click anything after the scene changes

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -2,10 +2,7 @@ extends CanvasLayer
 
 
 
-func _ready():
-	$KeyHUD.modulate = 100
-	$KeyHUD2.modulate = 100
-	$KeyHUD3.modulate = 100
+
 
 func _on_key_key_collected() -> void:
 	$KeyHUD.modulate = Color(1,1,1)
@@ -19,3 +16,18 @@ func _on_key_2_key_collected() -> void:
 
 func _on_key_3_key_collected() -> void:
 	$KeyHUD3.modulate = Color(1,1,1)
+
+
+func _on_level_1_tree_entered() -> void:
+	print("entered scene 1")
+	$KeyHUD.modulate = 200
+	$KeyHUD2.visible = false
+	$KeyHUD3.visible = false
+
+
+func _on_level_2_tree_entered() -> void:
+	$KeyHUD2.visible = true
+	$KeyHUD3.visible = true
+	$KeyHUD.modulate = 200
+	$KeyHUD2.modulate = 200
+	$KeyHUD3.modulate = 200
