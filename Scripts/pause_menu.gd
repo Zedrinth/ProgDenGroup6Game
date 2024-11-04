3081be5ad938e4e511df0e2c9dbe090e5aac1a21
George <georgehenrydeguzman@gmail.com>
2024-11-04 08:53:06 +0800

    Level1 Hitboxes

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -3,9 +3,9 @@ extends Control
 func _ready():
 	$AnimationPlayer.play("RESET")
 
-func _resume():
-	get_tree().paused = false
-	$AnimationPlayer.play("RESET")
+#func resume():
+#	get_tree().paused = false
+	
 
 func pause():
 	get_tree().paused = true
@@ -15,22 +15,20 @@ func escPause():
 	if Input.is_action_just_pressed("pause") and !get_tree().paused:
 		pause()
 	elif Input.is_action_just_pressed("pause") and get_tree().paused:
-		_resume()
+		get_tree().paused = false
+		$AnimationPlayer.play("RESET")
 
 func _on_resume_pressed():
-	_resume()
-
+	get_tree().paused = false
+	$AnimationPlayer.play("RESET")
 
 func _on_restart_pressed():
 	get_tree().reload_current_scene()
 
 
-func _on_options_pressed():
-	get_tree().change_scene_to_file("res://Scenes/options_pause_menu_l1.tscn")
-
 
 func _on_quit_to_menu_pressed():
-	_resume()
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 	MusicManager.lv1bgm.stop()
 	MusicManager.lv2bgm.stop()
