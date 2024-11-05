b9ee63237ccbb34f190741c6cbd2b4c1a14825e3
George <georgehenrydeguzman@gmail.com>
2024-11-05 09:40:15 +0800

    Update9

--- Scripts/door_level_1.gd
+++ Scripts/door_level_1.gd
@@ -3,11 +3,12 @@ extends Area2D
 var isOverDoor := false
 var isLocked := true
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if isOverDoor and Input.is_action_just_pressed("use") and !isLocked:
 		$AnimatedSprite2D.play("Opened")
-		
+		$EnimationOne.play("RESET")
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if $AnimatedSprite2D.animation == "Opened":
@@ -28,6 +29,7 @@ func _on_body_exited(body: Node2D) -> void:
 
 func _on_near_door_body_entered(body: Node2D) -> void:
 	if Global.keys == 1:
+		$EnimationOne.play("vanish")
 		$AnimatedSprite2D.play("Closed")
 		$AudioStreamPlayer.play()
 		isLocked = false
